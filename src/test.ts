a5ada96a60dc73489014a323e7a87dcab09af6f4
Valandur <inithilian@gmail.com>
2021-04-28 03:11:50 +0200

    Fix test

--- src/test.ts
+++ src/test.ts
@@ -3,11 +3,13 @@ const ray = require('raylib');
 ray.InitWindow(800, 480, 'main');
 ray.SetTargetFPS(30);
 
-const font = ray.LoadFont('./fonts/arial.ttf');
+const font = ray.LoadFont('./fonts/arial.fnt');
 
 const rec = { x: 100, y: 100, width: 600, height: 280 };
 
 while (!ray.WindowShouldClose()) {
+	const text = 'Hello world ' + new Date().getTime();
+
 	ray.BeginDrawing();
 	ray.ClearBackground(ray.BLACK);
 
@@ -15,7 +17,6 @@ while (!ray.WindowShouldClose()) {
 
 	ray.DrawRectangleLinesEx(rec, 1, ray.RED);
 
-	const text = 'Hello world ' + new Date().getTime();
 	ray.DrawTextRec(font, text, rec, 40, 1, true, ray.WHITE);
 
 	ray.EndDrawing();
