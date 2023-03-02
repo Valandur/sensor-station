d98de30ab9f83793a76916d4af13448ceb504ddc
Marco Crespi <mcrespi@tremor.co>
2023-03-02 15:04:58 +0100

    feat: add compiled new frontend

---
+++ frontend/build/_app/immutable/chunks/1.899e7f04.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.28f24c06.js";export{t as component};
---
+++ frontend/build/_app/immutable/chunks/3.43c60f76.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/settings-page.svelte.a26730dc.js";export{t as component};
