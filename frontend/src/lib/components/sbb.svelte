572b6ed942b1827ed83b21bc2320bbb2d008a193
Marco Crespi <me@valandur.io>
2023-03-28 23:03:59 +0200

    fix: make sbb component nicer

--- frontend/src/lib/components/sbb.svelte
+++ frontend/src/lib/components/sbb.svelte
@@ -19,32 +19,61 @@
 
 	$: rawAlerts = $store.data?.sbb.alerts || [];
 	$: index = getStore('alerts', rawAlerts.length);
-	$: alerts = [
-		...rawAlerts.slice($index, $index + MAX_ITEMS),
-		...rawAlerts.slice(0, Math.max(MAX_ITEMS - (rawAlerts.length - $index), 0))
-	];
+	$: alerts = [...rawAlerts.slice($index, $index + MAX_ITEMS)];
+
+	const formatDuration = (duration: string) => duration.replace('Dauer: ', '');
+	const formatLines = (lines: string) => lines.replace('Linien ', '');
+	const formatReason = (reason: string) => reason.replace('Grund: ', '');
 
 	onDestroy(async () => {
 		index.increment();
 	});
 </script>
 
-<div class="container-fluid h-100 m-0 d-flex flex-column" style="overflow-y: auto">
+<div class="container-fluid h-100 m-0 d-flex flex-column justify-content-end">
 	{#if alerts.length > 0}
-		{#each alerts as alert}
-			<div class="alert alert-warning m-1 p-2">
-				<p class="m-0">{alert.summary}</p>
-				<hr class="m-1" />
-				<p class="m-0">{alert.duration}</p>
-			</div>
-		{/each}
+		<div class="row row-cols-2">
+			{#each alerts as alert}
+				<div class="col">
+					<div class="card bg-warning border-warning bg-opacity-25">
+						<div class="card-header fw-bold small d-flex justify-content-between">
+							<div>{formatLines(alert.description)}</div>
+							<div><i class="icofont-clock-time" /> {formatDuration(alert.duration)}</div>
+						</div>
+						<div class="card-body">
+							<h5 class="card-title">{alert.summary}</h5>
+							<h6 class="card-subtitle mb-2 text-white text-opacity-50">
+								{formatReason(alert.reason)}
+							</h6>
+							<p class="card-text">{alert.consequence}</p>
+						</div>
+
+						<div class="card-arrow">
+							<div class="card-arrow-top-left" />
+							<div class="card-arrow-top-right" />
+							<div class="card-arrow-bottom-left" />
+							<div class="card-arrow-bottom-right" />
+						</div>
+					</div>
+				</div>
+			{/each}
+		</div>
 	{:else}
-		<div class="alert alert-info m-1 p-2">Keine Einschränkungen im ÖV in der Region Zürich</div>
+		<div class="row">
+			<div class="col">
+				<div class="card bg-success border-success bg-opacity-25">
+					<div class="card-body">
+						<h5 class="card-title mb-0">Keine Einschränkungen im ÖV in der Region Zürich</h5>
+					</div>
+
+					<div class="card-arrow">
+						<div class="card-arrow-top-left" />
+						<div class="card-arrow-top-right" />
+						<div class="card-arrow-bottom-left" />
+						<div class="card-arrow-bottom-right" />
+					</div>
+				</div>
+			</div>
+		</div>
 	{/if}
 </div>
-
-<style lang="scss">
-	.alert {
-		font-size: 1.5rem;
-	}
-</style>
