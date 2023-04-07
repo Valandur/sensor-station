403a8a0d7a2a3b6ad9e33ebdd1a155b577ab4559
Marco Crespi <me@valandur.io>
2023-04-08 00:09:19 +0200

    chore: update compiled files

--- frontend/build/_app/immutable/chunks/1.112d1c56.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.19d8fb56.js";export{t as component};
---
+++ frontend/build/_app/immutable/chunks/1.e89297c9.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.daa7b887.js";export{t as component};
